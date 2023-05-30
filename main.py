927c3edec1c6adc7d127537b7a7ccd345b87e3dc
POI <POI>
2023-05-30 10:26:50 +0900

    2020年以降のみ取得/出力先を年ごとに分ける/上場廃止と時価総額チェック処理を追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
-import os
+import itertools
 import time
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -19,6 +20,11 @@ def main():
     for stock_code in range(1000, 100000):
         # 対象外の証券コードの場合はスキップ
         if str(stock_code) in na_stock_code_list:
+            continue
+
+        # みんかぶから上場廃止/時価総額チェック
+        result = get_price(stock_code)
+        if not result:
             continue
 
         # IR BANKから日証金情報を取得
@@ -43,19 +49,22 @@ def get_data(stock_code, url):
     if table is None:
         with open('na_stock_code.csv', 'a', encoding = 'utf-8', newline = '') as f:
             writer = csv.writer(f, lineterminator = '\n')
-            writer.writerow([stock_code, time.strftime('%Y/%m/%d')])
+            writer.writerow([stock_code, time.strftime('%Y/%m/%d'), 'not exist'])
         return
 
     tbody = table.find('tbody')
     tr_list = tbody.find_all('tr')
 
-    years = -1
+    year = -1
 
     for tr in tr_list:
         # 年度記載列の場合年度を更新
         td_year = tr.find('td', class_ = 'ct')
         if not td_year is None:
-            years = int(td_year.text)
+            year = int(td_year.text)
+            # 2019年以前のデータは取らない
+            if year == 2019:
+                return
             continue
 
         # 年度記載以外の場合
@@ -74,7 +83,7 @@ def get_data(stock_code, url):
 
         # 各データ切り出し
         # 日付
-        date = f'{years}/{td_info[0].text}'
+        date = f'{year}/{td_info[0].text}'
         # 貸株残/増減
         kashikabu_zan = td_info[1].text.replace(',', '').replace('-', '+-').split('+')
         if len(kashikabu_zan) == 1: kashikabu_zan.append('0')
@@ -86,28 +95,13 @@ def get_data(stock_code, url):
         # 融資内訳(新規/返済)
         yushi_type = br_to_comma(td_info[4]).split(',')
 
-        # CSVファイルのパスとカラム名
-        csv_file = 'nisshokin_data.csv'
-
-        if file_created == False:
-            columns = ['stock_code', 'date', 'kashikabu', 'kashikabu_change', 'kashikabu_new', 'kashikabu_repay',
-                        'yushi', 'yushi_change', 'yushi_new', 'yushi_repay']
-
-            # CSVファイルが存在しない場合は新規作成し、カラム名を書き込む
-            if not os.path.exists(csv_file):
-                with open(csv_file, 'w', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(columns)
-            file_created = True
-
         # データをCSVに追記する
-        with open(csv_file, 'a', newline='') as file:
+        with open(f'nisshokin_data_{year}.csv', 'a', newline='') as file:
             writer = csv.writer(file)
 
             row = [stock_code, date, kashikabu_zan[0], kashikabu_zan[1], kashikabu_type[0], kashikabu_type[1],
                    yushi_zan[0], yushi_zan[1], yushi_type[0], yushi_type[1]]
             writer.writerow(row)
-
 
 def br_to_comma(content):
     '''bs4.element.Tag型のデータから改行をカンマに変え他のHTMLタグは取り除く'''
@@ -119,16 +113,53 @@ def br_to_comma(content):
             text += ','
     return text
 
+def get_price(stock_code):
+    '''みんかぶから上場廃止と時価総額を取ってくる'''
+    #time.sleep(2)
+    URL = f'https://minkabu.jp/stock/{stock_code}'
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'lxml')
+
+    # 上場廃止チェック
+    if '上場廃止</em>になりました' in str(soup):
+        with open('na_stock_code.csv', 'a', encoding = 'utf-8', newline = '') as f:
+            writer = csv.writer(f, lineterminator = '\n')
+            writer.writerow([stock_code, time.strftime('%Y/%m/%d'), 'delisting'])
+        return False
+
+    # 時価総額チェック
+    price = -1
+    tables = soup.find_all('table', class_ = 'md_table theme_light')
+    trs = list(itertools.chain.from_iterable([table.find_all('tr') for table in tables]))
+
+    for tr in trs:
+        price_match = re.search('時価総額(.+)百万円', tr.text)
+        if price_match != None:
+            price = price_match.groups()[0].replace(',', '')
+
+    # 取れなかったらエラー
+    if int(price) == -1:
+        pass # TODO エラー処理
+
+    # 500億以上は対象外
+    if int(price) >= 50000:
+        with open('na_stock_code.csv', 'a', encoding = 'utf-8', newline = '') as f:
+            writer = csv.writer(f, lineterminator = '\n')
+            writer.writerow([stock_code, time.strftime('%Y/%m/%d'), 'too big'])
+        return False
+
+    return True
+
+
 if __name__ == '__main__':
     main()
+    #get_price(7203) # ファストリ
+    #get_price(2172) # インサイト
 
 
-# TODO 容量削減のために2020年くらいまででリミッターかけとく？
-# memo L57 >= 2019でcontinue
 # TODO 二度走らせた時に同じデータを書き込まないように
 # memo 証券コード, 日付でユニーク
 ### TODO 毎度でかいcsvを読み込ませたくはない
 ### 年ごとにわけるか、重複防止用のユニークだけ記載したファイルを別途作るか
 ### TODO 一回でもユニークチェックに引っかかったらそれ以前の日付はすでに取得済みとみなしてcontinue
-# TODO 時価総額1000億以上の企業は除外リストに追加しておく(仕手は1000億には仕掛けないでしょ。。。500億でもいい気はするけど)
-# どっから取るかは要検討
+# TODO エラーが出たときのリカバリ処理(try-except)
